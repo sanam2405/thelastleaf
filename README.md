dc7b375ed728b0c4d7128f1c7d108a154f899b3f
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-06-20 13:50:07 +0530

    [feat]: initial vanilla setup

--- README.md
+++ README.md
@@ -1,30 +1,9 @@
-# React + TypeScript + Vite
+# thelastleaf
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+> _“Because I want to see the last leaf fall.
+> I have done enough waiting. I have done enough thinking. I want to go
+> sailing down, down, like one of those leaves.” ~ O. Henry_
 
-Currently, two official plugins are available:
+## _with_ ♥️ _for love by sanam_
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
-
-- Configure the top-level `parserOptions` property like this:
-
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
-
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+- [Manas Pratim Biswas](https://www.linkedin.com/in/manas-pratim-biswas)
--- vanilla/leafscript.js
+++ vanilla/leafscript.js
@@ -1,209 +1,207 @@
 const floatingImages = [];
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-1'),
-  image: document.getElementById('floating-image-1'),
+  container: document.getElementById("floating-image-container-1"),
+  image: document.getElementById("floating-image-1"),
   lastTime: 0,
   x: window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-2'),
-  image: document.getElementById('floating-image-2'),
+  container: document.getElementById("floating-image-container-2"),
+  image: document.getElementById("floating-image-2"),
   lastTime: 0,
   x: window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-3'),
-  image: document.getElementById('floating-image-3'),
+  container: document.getElementById("floating-image-container-3"),
+  image: document.getElementById("floating-image-3"),
   lastTime: 0,
   x: -window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-4'),
-  image: document.getElementById('floating-image-4'),
+  container: document.getElementById("floating-image-container-4"),
+  image: document.getElementById("floating-image-4"),
   lastTime: 0,
   x: -window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-5'),
-  image: document.getElementById('floating-image-5'),
+  container: document.getElementById("floating-image-container-5"),
+  image: document.getElementById("floating-image-5"),
   lastTime: 0,
   x: -window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-6'),
-  image: document.getElementById('floating-image-6'),
+  container: document.getElementById("floating-image-container-6"),
+  image: document.getElementById("floating-image-6"),
   lastTime: 0,
   x: window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-7'),
-  image: document.getElementById('floating-image-7'),
+  container: document.getElementById("floating-image-container-7"),
+  image: document.getElementById("floating-image-7"),
   lastTime: 0,
   x: window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-8'),
-  image: document.getElementById('floating-image-8'),
+  container: document.getElementById("floating-image-container-8"),
+  image: document.getElementById("floating-image-8"),
   lastTime: 0,
   x: -window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-9'),
-  image: document.getElementById('floating-image-9'),
+  container: document.getElementById("floating-image-container-9"),
+  image: document.getElementById("floating-image-9"),
   lastTime: 0,
   x: window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-10'),
-  image: document.getElementById('floating-image-10'),
+  container: document.getElementById("floating-image-container-10"),
+  image: document.getElementById("floating-image-10"),
   lastTime: 0,
   x: -window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-11'),
-  image: document.getElementById('floating-image-11'),
+  container: document.getElementById("floating-image-container-11"),
+  image: document.getElementById("floating-image-11"),
   lastTime: 0,
   x: -window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-12'),
-  image: document.getElementById('floating-image-12'),
+  container: document.getElementById("floating-image-container-12"),
+  image: document.getElementById("floating-image-12"),
   lastTime: 0,
   x: Math.random() * window.innerWidth,
   y: -Math.random() * window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-13'),
-  image: document.getElementById('floating-image-13'),
+  container: document.getElementById("floating-image-container-13"),
+  image: document.getElementById("floating-image-13"),
   lastTime: 0,
   x: -window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-14'),
-  image: document.getElementById('floating-image-14'),
+  container: document.getElementById("floating-image-container-14"),
+  image: document.getElementById("floating-image-14"),
   lastTime: 0,
   x: window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-15'),
-  image: document.getElementById('floating-image-15'),
+  container: document.getElementById("floating-image-container-15"),
+  image: document.getElementById("floating-image-15"),
   lastTime: 0,
   x: -window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-16'),
-  image: document.getElementById('floating-image-16'),
+  container: document.getElementById("floating-image-container-16"),
+  image: document.getElementById("floating-image-16"),
   lastTime: 0,
   x: window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-17'),
-  image: document.getElementById('floating-image-17'),
+  container: document.getElementById("floating-image-container-17"),
+  image: document.getElementById("floating-image-17"),
   lastTime: 0,
   x: -window.innerWidth,
   y: -window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-18'),
-  image: document.getElementById('floating-image-18'),
+  container: document.getElementById("floating-image-container-18"),
+  image: document.getElementById("floating-image-18"),
   lastTime: 0,
   x: -window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-19'),
-  image: document.getElementById('floating-image-19'),
+  container: document.getElementById("floating-image-container-19"),
+  image: document.getElementById("floating-image-19"),
   lastTime: 0,
   x: window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-20'),
-  image: document.getElementById('floating-image-20'),
+  container: document.getElementById("floating-image-container-20"),
+  image: document.getElementById("floating-image-20"),
   lastTime: 0,
   x: Math.random() * window.innerWidth,
   y: -Math.random() * window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
 
 floatingImages.push({
-  container: document.getElementById('floating-image-container-21'),
-  image: document.getElementById('floating-image-21'),
+  container: document.getElementById("floating-image-container-21"),
+  image: document.getElementById("floating-image-21"),
   lastTime: 0,
   x: window.innerWidth,
   y: window.innerHeight,
-  angle: Math.random() * 360
+  angle: Math.random() * 360,
 });
-
 
 function moveImages(currentTime) {
   floatingImages.forEach((image) => {
-    const timeDelta = (currentTime - image.lastTime);
+    const timeDelta = currentTime - image.lastTime;
     const smallScreen = window.matchMedia("(max-width: 767px)").matches;
     image.lastTime = currentTime;
 
     if (!image.isPaused) {
-
       if (smallScreen) {
-        image.x += Math.sin(image.angle * Math.PI / 180) * timeDelta / 35;
-        image.y += Math.cos(image.angle * Math.PI / 180) * timeDelta / 35;
+        image.x += (Math.sin((image.angle * Math.PI) / 180) * timeDelta) / 35;
+        image.y += (Math.cos((image.angle * Math.PI) / 180) * timeDelta) / 35;
       } else {
-        image.x += Math.sin(image.angle * Math.PI / 180) * timeDelta / 10;
-        image.y += Math.cos(image.angle * Math.PI / 180) * timeDelta / 10;
+        image.x += (Math.sin((image.angle * Math.PI) / 180) * timeDelta) / 10;
+        image.y += (Math.cos((image.angle * Math.PI) / 180) * timeDelta) / 10;
       }
       image.angle += 0.1;
     }
@@ -211,7 +209,7 @@ function moveImages(currentTime) {
     // Check if the image has gone off the right edge of the screen
     if (image.x > window.innerWidth || image.y > window.innerHeight) {
       // Smoothly hide the image
-      image.container.style.opacity = '0.1';
+      image.container.style.opacity = "0.1";
 
       const getRandomDirection = Math.ceil(Math.random() * 10) % 4;
 
@@ -234,12 +232,12 @@ function moveImages(currentTime) {
 
       // Smoothly show the image
       setTimeout(() => {
-        image.container.style.opacity = '0.9';
-        image.container.style.transition = 'opacity 1s ease-in-out';
+        image.container.style.opacity = "0.9";
+        image.container.style.transition = "opacity 1s ease-in-out";
       }, 1000);
     }
 
-    const opacity = 0.85 + 0.10 * Math.sin(currentTime / 1000);
+    const opacity = 0.85 + 0.1 * Math.sin(currentTime / 1000);
     image.container.style.opacity = opacity;
     image.image.style.transform = `translate(${image.x}px, ${image.y}px) rotate(${image.angle}deg)`;
   });
@@ -251,20 +249,20 @@ floatingImages.forEach((image) => {
   image.isPaused = false;
 
   // Pause the image movement when hovering or touching
-  image.container.addEventListener('mouseenter', () => {
+  image.container.addEventListener("mouseenter", () => {
     image.isPaused = true;
   });
 
-  image.container.addEventListener('touchstart', () => {
+  image.container.addEventListener("touchstart", () => {
     image.isPaused = true;
   });
 
   // Resume the image movement when the hover or touch ends
-  image.container.addEventListener('mouseleave', () => {
+  image.container.addEventListener("mouseleave", () => {
     image.isPaused = false;
   });
 
-  image.container.addEventListener('touchend', () => {
+  image.container.addEventListener("touchend", () => {
     image.isPaused = false;
   });
 });
